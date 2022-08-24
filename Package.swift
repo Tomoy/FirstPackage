2fea496925b5f8337fd040f7c1698e2e2b7c35c6
Tomás <moyanotomasi@gmail.com>
2022-08-24 19:59:04 +0200

    Test

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FirstPackage",
-            targets: ["FirstFramework"]),
+            targets: ["FirstPackage"]),
     ],
     dependencies: [
         .package(url: "https://github.com/maxkonovalov/MKRingProgressView.git", from: "2.3.0")
